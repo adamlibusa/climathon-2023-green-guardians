c306c8f14799b5422048feac4542e807da7a6bf1
Filip Šipoš <112976056+filipcodes@users.noreply.github.com>
2023-10-21 14:18:13 +0200

    Frontend Init
    Vite initialization, File structure, Vue Router and Component tree established, stateManager (js) created.

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style lang="scss">
+@import "@/assets/styles/reset.scss";
+@import "@/assets/styles/abstracts.scss";
+
+body {
+  background-color: $body-background-color;
+  font-family: "SfPro-R", sans-serif;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <main>Home View</main>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss"></style>
